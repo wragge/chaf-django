e9b459a973a9ed55f7593439733b57636a3af956
Tim Sherratt <tim@discontents.com.au>
2014-03-30 12:55:33 +1100

    Tweaking rdf output

--- chaf/tungwah/views.py
+++ chaf/tungwah/views.py
@@ -103,7 +103,15 @@ class IssueView(NewspaperView):
         graph.add((this_issue, namespaces['schema']['datePublished'], Literal(str(entity['issue']))))
         graph.add((this_issue, namespaces['schema']['provider'], this_newspaper))
         for article in entity['articles']:
-            graph.add((this_issue, namespaces['dcterms']['hasPart'], URIRef(host_ns[article.get_absolute_url()])))
+            this_article = URIRef(host_ns[article.get_absolute_url()])
+            graph.add((this_article, namespaces['rdf']['type'], namespaces['schema']['NewsArticle']))
+            graph.add((this_article, namespaces['rdfs']['label'], Literal(str(article))))
+            graph.add((this_article, namespaces['schema']['name'], Literal(str(article))))
+            graph.add((this_issue, namespaces['dcterms']['hasPart'], this_article))
+            graph.add((this_article, namespaces['schema']['description'], Literal(str(article.description))))
+            graph.add((this_article, namespaces['schema']['printPage'], Literal(str(article.page))))
+            graph.add((this_article, namespaces['schema']['printColumn'], Literal(str(article.page_column))))
+            graph.add((this_article, namespaces['schema']['datePublished'], Literal(str(article.issue_date))))
         return graph
 
 class ArticleListView(LinkedDataListView):
@@ -150,6 +158,11 @@ class ArticleSearchView(LinkedDataSearchView):
             this_article = URIRef(host_ns[article.object.get_absolute_url()])
             graph.add((this_article, namespaces['rdfs']['label'], Literal(str(article.object))))
             graph.add((this_article, namespaces['schema']['name'], Literal(str(article.object))))
+            graph.add((this_issue, namespaces['dcterms']['hasPart'], this_article))
+            graph.add((this_article, namespaces['schema']['description'], Literal(str(article.object.description))))
+            graph.add((this_article, namespaces['schema']['printPage'], Literal(str(article.object.page))))
+            graph.add((this_article, namespaces['schema']['printColumn'], Literal(str(article.object.page_column))))
+            graph.add((this_article, namespaces['schema']['datePublished'], Literal(str(article.object.issue_date))))
             if article.object.issue_date < datetime.date(1902, 8, 16):
                 graph.add((news, namespaces['dcterms']['hasPart'], this_article))
             else:
